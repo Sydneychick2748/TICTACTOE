d021bfc8ecdc0f43ad0eb89c53ac86e2977d3d94
ana <ana@anniecannons.com>
2023-11-30 11:24:36 -0800

    finished this tictactoe

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
-import Board from "./Components/Board"
+import Game from "./Components/Game"
 
 function App() {
   return (
     <div className="App">
+    
         <h1 className="board-title">TIC TAC TOE</h1>
-      <Board/>
+      <Game/>
 
       
     </div>
--- src/Components/Board.js
+++ src/Components/Board.js
@@ -2,29 +2,61 @@ import "../App.css";
 import Square from "../Components/Square";
 import { useState } from "react";
 
-function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setxIsNext ] =useState(true);
+function Board({squares, onPlay, isNext}) {
+  
+//Display winner
+const winner = calculateWinner(squares);
+
+let status;
+if (winner) {
+  status = "Winner: " + winner;
+} else {
+  status = "Next player: " + (isNext ? "X" : "O");
+}
 
 
-
-  function handleClick(i) {
-    const nextSquares = squares.slice();
+  function handleClick(index) {
+    if(squares[index] || calculateWinner(squares)){
+      return
+    }
+    const nextSquares = [...squares];
  
-    if(xIsNext){
-      nextSquares[i]= "X"
+    if(isNext){
+      nextSquares[index]= "X"
     }else{
-      nextSquares[i]="O"
+      nextSquares[index]="O"
     }
     
-    setSquares(nextSquares)
-    setxIsNext(!xIsNext)
-    
+    onPlay(nextSquares)
     console.log(nextSquares, "clicked");
+  }
+
+
+
+
+  function calculateWinner(squares){
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
   }
 
   return (
     <div>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={()=>handleClick(0)} />
         <Square value={squares[1]} onSquareClick={()=>handleClick(1)} />
